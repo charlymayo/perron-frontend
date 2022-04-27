fa51f087f1afd1958b322812ae5ea4b0a5157680
charlymayo <charlymayomail@gmail.com>
2022-04-27 12:22:26 -0500

    set marker's icon

--- src/app/location.component.ts
+++ src/app/location.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+declare var google: any;
 
 @Component({
   selector: 'location',
@@ -9,4 +10,13 @@ export class LocationComponent {
     latitude: 52.228973,
     longitude: 20.728218
   }
+  icon = {
+    url: "assets/pawicon.png",
+    // This marker is 20 pixels wide by 32 pixels high.
+    size: new google.maps.Size(20, 32),
+    // The origin for this image is (0, 0).
+    origin: new google.maps.Point(0, 0),
+    // The anchor for this image is the base of the flagpole at (0, 32).
+    anchor: new google.maps.Point(0, 32),
+  };
 }
