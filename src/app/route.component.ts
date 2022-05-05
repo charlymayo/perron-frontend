f88fb5bec249eb59cc6643c4f2528a93356b3ad2
charlymayo <charlymayomail@gmail.com>
2022-05-04 19:11:51 -0500

    Sort & filter routes

--- src/app/route.component.ts
+++ src/app/route.component.ts
@@ -10,23 +10,42 @@ export class RouteComponent {
   routeUrlBase:string = "https://static-maps.yandex.ru/1.x/?lang=en_US&l=map&pl="; 
   startTime = {hour: 0, minute: 0};
   endTime = {hour: 23, minute: 59};
+  routeImgSrc:string = "";
 
-  getRoute()
-  {
+  positions = [{'latitude': '21.870028', 'longitude': '-102.279372', 'time': '10:28'}, {'latitude': '21.850008', 'longitude': '-102.272362', 'time': '07:00'}, {'latitude': '21.880328', 'longitude': '-102.272572', 'time': '20:28'}, {'latitude': '21.873028', 'longitude': '-102.254372', 'time': '20:27'}];
 
-  }
+  filteredPositions = this.positions;
 
-  positions = [{'latitude': '21.870028', 'longitude': '-102.279372', 'time': '10:28'}, {'latitude': '21.850008', 'longitude': '-102.272362', 'time': '7:00'}, {'latitude': '21.880328', 'longitude': '-102.272572', 'time': '20:28'}, {'latitude': '21.873028', 'longitude': '-102.254372', 'time': '22:30'}];
-
-  routeImgSrc = "";
-  constructor(){
-    if(this.routeImgSrc === ""){
-      this.routeImgSrc = this.routeUrlBase;
-    }
-    for (var position of this.positions) {
+  getRoute(){
+    this.routeImgSrc = this.routeUrlBase;
+    for (var position of this.filteredPositions) {
       this.routeImgSrc += position.longitude + "," + position.latitude;
       this.routeImgSrc += ",";
     }
     this.routeImgSrc = this.routeImgSrc.slice(0, -1);
+  }
+
+  filterRoute(){
+    this.positions.sort(function (a, b) {
+      return a.time.localeCompare(b.time);
+    });
+
+    var hourStr:string = (this.startTime.hour.toString().length == 1) ? "0" + this.startTime.hour.toString() : this.startTime.hour.toString();
+    var minuteStr:string = (this.startTime.minute.toString().length == 1) ? "0" + this.startTime.minute.toString() : this.startTime.minute.toString();
+    var startTimeStr:string = hourStr + ":" + minuteStr;
+
+    hourStr = (this.endTime.hour.toString().length == 1) ? "0" + this.endTime.hour.toString() : this.endTime.hour.toString();
+    minuteStr = (this.endTime.minute.toString().length == 1) ? "0" + this.endTime.minute.toString() : this.endTime.minute.toString();
+    var endTimeStr:string = hourStr + ":" + minuteStr;
+
+    this.filteredPositions = this.positions.filter(function(a){
+      return a.time.localeCompare(startTimeStr) > -1 && a.time.localeCompare(endTimeStr) < 1;
+    });
+
+    this.getRoute();
+  }
+
+  constructor(){
+    this.getRoute();
   }
 }
